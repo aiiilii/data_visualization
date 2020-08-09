6e14703a7f9dde8b888ad3ec4cd24e2bde42721d
Aili Gong <aili.hgong@gmail.com>
2020-08-09 10:54:21 -0700

    Added hacker news submissions

--- hn_article.py
+++ hn_article.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# make an APi call, and store the response.
+# Make an APi call, and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
